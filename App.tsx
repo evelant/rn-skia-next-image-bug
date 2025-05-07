2df097be1eb80bab214858dd63d0f837fd8a9afd
evelant <andrew@taskheroics.com>
2025-05-07 10:28:35 -0400

    example of broken behavior with images

--- App.tsx
+++ App.tsx
@@ -1,18 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {Canvas, useImage, Image} from '@shopify/react-native-skia';
+import _ from "lodash"
+import React from "react";
 
 export default function App() {
+    const [toggle, setToggle] = React.useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+        <WorkingImages />
+        {toggle ? <BrokenImages /> : null}
       <StatusBar style="auto" />
+        <Button title={"toggle 2nd example"} onPress={() => setToggle(!toggle)} />
+
     </View>
   );
+}
+
+const WorkingImages = () => {
+    const images = [2,0,1].map(i => ({
+        zIndex: i,
+        img: <TestImage key={i} index={i} />
+    }))
+    return (
+        <View style={styles.container}>
+            <Text>This works, but only after first re-render, toggle to see</Text>
+            <Canvas style={{width: 200, height: 200, backgroundColor: "red"}}>
+                {images.map(i => i.img)}
+            </Canvas>
+        </View>
+
+    )
+}
+
+const BrokenImages = () => {
+    const images = [2,0,1].map(i => ({
+        zIndex: i,
+        img: <TestImage key={i} index={i} />
+    }))
+    return (
+        <View style={styles.container}>
+            <Text>This does not work, no images are rendered regardless of other test component being rendered (but oddly, will render after a fast refresh)</Text>
+            <Canvas style={{width: 200, height: 200, backgroundColor: "cyan"}}>
+                <>{_.sortBy(images, "zIndex").map(i => i.img)}</>
+            </Canvas>
+        </View>
+
+    )
+}
+
+
+
+const TestImage = ({index}: {index: number}) => {
+  const image = useImage(require('./assets/icon.png'));
+
+  return <Image image={image} x={index * 50} y={index * 50}  width={100} height={100} />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+      padding: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
---
+++ README.md
@@ -0,0 +1,13 @@
+# react-native-skia 2.0.0-next.3 image bug
+
+Run the example app (only tested on iOS). You will see the following strange behavior with images:
+
+* Images in the first example don't render until a re-render is triggered
+* Images in the second example don't render at all, the only difference being a sort of the array
+* Images in the second example will only render after a fast refresh
+
+The behavior seems strange and inconsistent. I'm unsure whether this nails down an exact reproduction but it is as simple as I could make it.
+
+In my actual app I have a similar component rendering a bunch of images layered on top of each other so they can be snapshot as a single png.
+
+Everything worked on 1.12.4 on expo 52 and the old arch. This issue appeared with upgrade to 2.0.0-next.x, expo 53, and enabling the new arch.
